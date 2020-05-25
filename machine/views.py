731e84a0df710d06e595892dbb515213e1079136
TriNguyen <tlnguyen@ualberta.ca>
2020-05-25 11:59:26 -0600

    We can now search all options

--- machine/views.py
+++ machine/views.py
@@ -13,10 +13,54 @@ class SearchResultsView(ListView):
 
 	def get_queryset(self):
 		query_hostname = self.request.GET.get('q_hostname')
+		
 		query_ipv4 = self.request.GET.get('q_ipv4')
+		query_location = self.request.GET.get('q_location')
+		query_ipv6 = self.request.GET.get('q_ipv6')
+		query_os = self.request.GET.get('q_os')
+		query_physical_virtual = self.request.GET.get("q_physical_virtual")
+		query_owner = self.request.GET.get('q_owner')
+		query_administrator = self.request.GET.get('q_administrator')
+		query_uofa_tag = self.request.GET.get('q_uofa_tag')
+		query_model = self.request.GET.get('q_model')
+		query_processor = self.request.GET.get('q_processor')
+		query_ram = self.request.GET.get('q_ram')
+		query_storage = self.request.GET.get('q_storage')
+		query_gpu = self.request.GET.get('q_gpu')
+		query_serial_number = self.request.GET.get('q_serial_number')
+		query_status = self.request.GET.get('q_status')
+		query_rack = self.request.GET.get('q_rack')
+		query_powerup = self.request.GET.get('q_powerup')
+		query_support = self.request.GET.get('q_support')
+		query_department = self.request.GET.get('q_department')
+		query_srit_access = self.request.GET.get('q_srit_access')
+		query_comments = self.request.GET.get('q_comments')
+
+
+
 		return Machines.objects.filter(
 			Q(hostname__icontains=query_hostname) &
-			Q(ipv4__icontains=query_ipv4)
+			Q(ipv4__icontains=query_ipv4) &
+			Q(last_known_location__icontains=query_location) &
+			Q(ipv6__icontains=query_ipv6) &
+			Q(operating_system__icontains=query_os) &
+			Q(physical_virtual_machine__icontains=query_physical_virtual) &
+			Q(owner__icontains=query_owner) &
+			Q(administrator__icontains=query_administrator) &
+			Q(u_of_a_tag_number__icontains=query_uofa_tag) &
+			Q(make_model__icontains=query_model) &
+			Q(processor__icontains=query_processor) &
+			Q(ram_gb_field__icontains=query_ram) &
+			Q(storage_space_gb_field__icontains=query_storage) &
+			Q(gpu__icontains=query_gpu) &
+			Q(serial_number__icontains=query_serial_number) &
+			Q(status__icontains=query_status) &
+			Q(rack_number__icontains=query_rack) &
+			Q(power_up__icontains=query_powerup) &
+			Q(support_team__icontains=query_support) &
+			Q(department__icontains=query_department) &
+			Q(srit_access__icontains=query_srit_access) &
+			Q(comments__icontains=query_comments)
 		)
 
 def machine_edit(request, pk):
