e02ca2ce83b58a2e9840cf5887e80e7e894bd78d
TriNguyen <tlnguyen@ualberta.ca>
2020-05-24 23:20:41 -0600

    Added ALLOWED_HOST

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^eaj=4#8ysacjw^n_qma-u^)vyelio1=pga8xytai-)wc1pe#1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tlnguyen.pythonanywhere.com']
 
 
 # Application definition
