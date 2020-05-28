b541b2edec67a7c2974452bbf0d66927f06b0164
TriNguyen <tlnguyen@ualberta.ca>
2020-05-27 21:43:10 -0600

    External CSS files

--- src/settings.py
+++ src/settings.py
@@ -124,5 +124,6 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
--- src/urls.py
+++ src/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import home_page
+
 urlpatterns = [
 	path('', home_page, name='home'),
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
